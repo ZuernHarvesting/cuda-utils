d4464ce1c40d99a48e2462bc8f2ccbc37df8169d
Tom Stirnkorb <tom.stirnkorb@zuern.de>
2022-08-26 11:46:20 +0200

    make it header only

--- include/cuda-utils.cuh
+++ include/cuda-utils.cuh
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <stdio.h>
 namespace cudautils
 {
-    void distancePointToPlane(float3 point, float4 plane);
+    __device__ float distancePointToPlane(float3 point, float4 plane)
+    {
+        return (plane.x*point.x + plane.y*point.y + plane.z*point.z + plane.w);
+    }
+    __device__ float distancePointToPlane(float4 point, float4 plane)
+    {
+        return (plane.x*point.x + plane.y*point.y + plane.z*point.z + plane.w);
+    }
 }
